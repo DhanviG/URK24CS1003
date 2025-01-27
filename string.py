92ab1273be53d98ff691b8a6efa1fe1297e15a2d
DhanviG <dhanvig@karunya.edu.in>
2025-01-27 23:23:51 +0530

    I want to modify because I deleted the data

--- string.py
+++ string.py
@@ -1,6 +1,1 @@
-def count_words(input_string):
-    words = input_string.split()
-    return len(words)
-input_string = "Hello, how are you doing today?"
-word_count = count_words(input_string)
-print(f"The number of words in the string is: {word_count}")
+
